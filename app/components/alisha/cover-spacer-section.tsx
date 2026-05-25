a40173244a70112b8a8e3316f87228465f978a90
Dani Surya <danisuryabintangpratama@gmail.com>
2026-05-25 19:29:50 +0700

    Remove Elementor traces from cover sections

--- app/components/alisha/cover-spacer-section.tsx
+++ app/components/alisha/cover-spacer-section.tsx
@@ -1,10 +1,8 @@
 export function CoverSpacerSection() {
   return (
     <div
-      className="elementor-element elementor-element-6715487c e-con-full elementor-hidden-mobile elementor-hidden-tablet e-flex e-con e-child elementor-sticky elementor-sticky__spacer"
+      className="gc-element gc-element-6715487c e-con-full elementor-hidden-mobile elementor-hidden-tablet e-flex e-con e-child elementor-sticky elementor-sticky__spacer gc-sticky__spacer"
       data-id="6715487c"
-      data-element_type="container"
-      data-e-type="container"
       data-settings='{"sticky":"top","sticky_on":["desktop","tablet","mobile"],"sticky_offset":0,"sticky_effects_offset":0,"sticky_anchor_link_offset":0}'
       style={{
         visibility: "hidden",
@@ -13,48 +11,35 @@ export function CoverSpacerSection() {
       }}
     >
       <div
-        className="elementor-element elementor-element-49714b67 e-con-full elementor-hidden-mobile e-flex e-con e-child"
+        className="gc-element gc-element-49714b67 e-con-full elementor-hidden-mobile e-flex e-con e-child"
         data-id="49714b67"
-        data-element_type="container"
-        data-e-type="container"
         data-settings='{"background_background":"classic"}'
       >
         <div
-          className="elementor-element elementor-element-f5c6215 e-con-full e-flex e-con e-child"
+          className="gc-element gc-element-f5c6215 e-con-full e-flex e-con e-child"
           data-id="f5c6215"
-          data-element_type="container"
-          data-e-type="container"
         >
           <div
-            className="elementor-element elementor-element-4e956f02 elementor-widget elementor-widget-heading"
+            className="gc-element gc-element-4e956f02 gc-widget gc-widget-heading"
             data-id="4e956f02"
-            data-element_type="widget"
-            data-e-type="widget"
-            data-widget_type="heading.default"
           >
-            <h2 className="elementor-heading-title elementor-size-default">
+            <h2 className="gc-heading-title gc-size-default">
               UNDANGAN PERNIKAHAN
             </h2>
           </div>
           <div
-            className="elementor-element elementor-element-a04b776 elementor-widget elementor-widget-heading"
+            className="gc-element gc-element-a04b776 gc-widget gc-widget-heading"
             data-id="a04b776"
-            data-element_type="widget"
-            data-e-type="widget"
-            data-widget_type="heading.default"
           >
-            <h2 className="elementor-heading-title elementor-size-default">
+            <h2 className="gc-heading-title gc-size-default">
               Rangga & Cinta
             </h2>
           </div>
           <div
-            className="elementor-element elementor-element-7e79c14a elementor-widget elementor-widget-heading"
+            className="gc-element gc-element-7e79c14a gc-widget gc-widget-heading"
             data-id="7e79c14a"
-            data-element_type="widget"
-            data-e-type="widget"
-            data-widget_type="heading.default"
           >
-            <h2 className="elementor-heading-title elementor-size-default">
+            <h2 className="gc-heading-title gc-size-default">
               10 Januari 2025
             </h2>
           </div>
@@ -63,3 +48,4 @@ export function CoverSpacerSection() {
     </div>
   );
 }
+
