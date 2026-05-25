d919bbe31018b851cf58146cfe43216dbf6c3bef
Dani Surya <danisuryabintangpratama@gmail.com>
2026-05-25 19:37:27 +0700

    Remove Elementor traces from main wrappers

--- app/components/alisha/elementor-main-start.ts
+++ app/components/alisha/elementor-main-start.ts
@@ -1,3 +1,4 @@
 export function ElementorMainStart() {
-  return "<div data-elementor-type=\"wp-post\" data-elementor-id=\"45286\" class=\"elementor elementor-45286\" data-elementor-post-type=\"post\">\n\t\t\t\t<div class=\"elementor-element elementor-element-6baf189a e-con-full e-flex e-con e-parent\" data-id=\"6baf189a\" data-element_type=\"container\" data-e-type=\"container\">\n\t\t";
+  return "<div class=\"elementor elementor-45286 gc-elementor gc-elementor-45286\" data-id=\"45286\">\n\t\t\t\t<div class=\"gc-element gc-element-6baf189a e-con-full e-flex e-con e-parent\" data-id=\"6baf189a\">\n\t\t";
 }
+
